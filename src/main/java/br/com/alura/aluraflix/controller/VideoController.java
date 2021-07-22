ffaab31ef80f8f0c3137beb23b967f0ffd70fec6
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-21 21:01:27 -0300

    adiciona endpoint get /v1/videos/{id} para buscar video por id

--- src/main/java/br/com/alura/aluraflix/controller/VideoController.java
+++ src/main/java/br/com/alura/aluraflix/controller/VideoController.java
@@ -2,11 +2,14 @@ package br.com.alura.aluraflix.controller;
 
 import br.com.alura.aluraflix.entity.Video;
 import br.com.alura.aluraflix.repository.VideoRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/videos")
@@ -21,5 +24,12 @@ public class VideoController {
     @GetMapping
     public List<Video> listarTodosVideos() {
         return repository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Video> listarVideoPorId(@PathVariable("id") Long id) {
+        Optional<Video> video = repository.findById(id);
+        if (video.isPresent()) return ResponseEntity.ok(video.get());
+        return ResponseEntity.notFound().build();
     }
 }
