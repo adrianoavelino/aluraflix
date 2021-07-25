0184453b3d8e71d10b15e21168087f78ad0fc71a
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-25 16:13:56 -0300

    adiciona banco de dados h2 no ambiente de testes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO videos(descricao, titulo, url) VALUES ("Domain Driven Design, o que ? Por que foi criada e qual objetivo dessa linguagem dentro da programao?", "Domain Driven Design com Alberto Sousa, o Dev Eficiente", "https://www.youtube.com/watch?v=n40Z1c9Ryog");
-INSERT INTO videos(descricao, titulo, url) VALUES ("Do básico ao avançado com Mysql", "Mysql", "https://www.meusite.com.br/mysql");
-INSERT INTO videos(descricao, titulo, url) VALUES ("Primeiros passos com docker", "Dcoker", "https://www.meusite.com.br/docker");
+INSERT INTO videos(id, descricao, titulo, url) VALUES (1, 'Domain Driven Design, o que ? Por que foi criada e qual objetivo dessa linguagem dentro da programao?', 'Domain Driven Design com Alberto Sousa, o Dev Eficiente', 'https://www.youtube.com/watch?v=n40Z1c9Ryog');
+INSERT INTO videos(id, descricao, titulo, url) VALUES (2, 'Do básico ao avançado com Mysql', 'Mysql', 'https://www.meusite.com.br/mysql');
+INSERT INTO videos(id, descricao, titulo, url) VALUES (3, 'Primeiros passos com docker', 'Docker', 'https://www.meusite.com.br/docker');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,4 +6,4 @@ CREATE TABLE `videos` (
   `titulo` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
