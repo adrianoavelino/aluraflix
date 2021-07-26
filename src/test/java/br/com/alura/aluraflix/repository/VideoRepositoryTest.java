e3cc0e449ca2492765608779f6783acc00328c1e
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-26 11:54:06 -0300

    adiciona testes unitários para validação do repository

--- src/test/java/br/com/alura/aluraflix/repository/VideoRepositoryTest.java
+++ src/test/java/br/com/alura/aluraflix/repository/VideoRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.validation.ConstraintViolationException;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class VideoRepositoryTest {
@@ -24,6 +26,33 @@ class VideoRepositoryTest {
     }
 
     @Test
+    void deveMostrarErrosWhenSalvarVideoComDadosInvalidos() {
+        Video video = new Video("", "", "");
+
+        Throwable throable = org.junit.jupiter.api.Assertions.assertThrows(
+                ConstraintViolationException.class,
+                () -> this.repository.save(video)
+        );
+
+        Assertions.assertThat(throable).isInstanceOf(ConstraintViolationException.class);
+        Assertions.assertThat(throable.getMessage()).contains("precisa ter no mínimo 3 letras");
+        Assertions.assertThat(throable.getMessage()).contains("Ops, você esqueceu da URL");
+    }
+
+    @Test
+    void deveMostrarErroQuandoSalvarVideoComUrlInvalida() {
+        Video video = new Video("TDD", "Teste Unitário", "urlinvalida.com.br");
+
+        Throwable throwable = org.junit.jupiter.api.Assertions.assertThrows(
+                ConstraintViolationException.class,
+                () -> this.repository.save(video)
+        );
+
+        Assertions.assertThat(throwable).isInstanceOf(ConstraintViolationException.class);
+        Assertions.assertThat(throwable.getMessage()).contains("Você deve usar uma url válida. Ex: http://www.site.com.br");
+    }
+
+    @Test
     void deveAtualizarVideo() {
         Video video = new Video("TDD", "Teste Unitário", "http://www.tdd.com.br");
         this.repository.save(video);
@@ -34,6 +63,42 @@ class VideoRepositoryTest {
         Video videoAtual = this.repository.findById(video.getId()).get();
 
         Assertions.assertThat(videoAtual).usingRecursiveComparison().isEqualTo(videoAlterado);
+    }
+
+    @Test
+    void deveMostrarErrosWhenAtualizarVideoComDadosInvalidos() {
+        Video video = new Video("TDD", "Teste Unitário", "http://www.tdd.com.br");
+        this.repository.save(video);
+        Video videoAlterado = new Video(video.getId(), "", "", "");
+
+        Throwable throwable = org.junit.jupiter.api.Assertions.assertThrows(
+            ConstraintViolationException.class,
+            () -> {
+                try {
+                    this.repository.save(videoAlterado);
+                } catch (Throwable e) {
+                    throw e.getCause().getCause();
+                }
+            }
+        );
+
+        Assertions.assertThat(throwable).isInstanceOf(ConstraintViolationException.class);
+        Assertions.assertThat(throwable.getMessage()).contains("precisa ter no mínimo 3 letras");
+        Assertions.assertThat(throwable.getMessage()).contains("Ops, você esqueceu da URL");
+    }
+
+    @Test
+    void deveMostrarErroQuandoAtualizarVideoComUrlInvalida() {
+        Video video = new Video("TDD", "Teste Unitário", "http://www.tdd.com.br");
+        Video videoAlterado = new Video(video.getId(), "TDD Atualizado", "Teste Unitário", "urlinvalida");
+
+        Throwable throwable = org.junit.jupiter.api.Assertions.assertThrows(
+            ConstraintViolationException.class,
+            () -> this.repository.save(videoAlterado)
+        );
+
+        Assertions.assertThat(throwable).isInstanceOf(ConstraintViolationException.class);
+        Assertions.assertThat(throwable.getMessage()).contains("Você deve usar uma url válida. Ex: http://www.site.com.br");
     }
 
     @Test
