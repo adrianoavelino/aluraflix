ede369c274673369250a66f836323d12063c9253
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-27 21:58:24 -0300

    adiciona a entity categoria

---
+++ src/main/java/br/com/alura/aluraflix/entity/Categoria.java
@@ -0,0 +1,46 @@
+package br.com.alura.aluraflix.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String cor;
+
+    public Categoria(String titulo, String cor) {
+        this.titulo = titulo;
+        this.cor = cor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
