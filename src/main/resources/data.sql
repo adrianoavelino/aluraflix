191ceec3416ba59f7382855f0ec23f49e17dedb9
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-29 23:31:04 -0300

    atualiza schema.sql e data.sql
    atualiza inicialização dos dados do banco de dados com a estrutura do 
    relacionamento de video e categoria

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
-INSERT INTO videos(id, descricao, titulo, url) VALUES (1, 'Domain Driven Design, o que ? Por que foi criada e qual objetivo dessa linguagem dentro da programao?', 'Domain Driven Design com Alberto Sousa, o Dev Eficiente', 'https://www.youtube.com/watch?v=n40Z1c9Ryog');
-INSERT INTO videos(id, descricao, titulo, url) VALUES (2, 'Do básico ao avançado com Mysql', 'Mysql', 'https://www.meusite.com.br/mysql');
-INSERT INTO videos(id, descricao, titulo, url) VALUES (3, 'Primeiros passos com docker', 'Docker', 'https://www.meusite.com.br/docker');
+INSERT INTO categorias(id, cor, titulo) VALUES (1, '#000000', 'LIVRE');
+INSERT INTO categorias(id, cor, titulo) VALUES (2, '#FFFFFFF', 'PROGRAMAÇÃO');
+INSERT INTO categorias(id, cor, titulo) VALUES (3, '#CCCCCC', 'COMIDA');
+
+INSERT INTO videos(id, descricao, titulo, url, categoria_id) VALUES (1, 'Domain Driven Design, o que ? Por que foi criada e qual objetivo dessa linguagem dentro da programao?', 'Domain Driven Design com Alberto Sousa, o Dev Eficiente', 'https://www.youtube.com/watch?v=n40Z1c9Ryog', 1);
+INSERT INTO videos(id, descricao, titulo, url, categoria_id) VALUES (2, 'Do básico ao avançado com Mysql', 'Mysql', 'https://www.meusite.com.br/mysql', 1);
+INSERT INTO videos(id, descricao, titulo, url, categoria_id) VALUES (3, 'Primeiros passos com docker', 'Docker', 'https://www.meusite.com.br/docker', 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,21 @@
 DROP TABLE IF EXISTS videos;
+DROP TABLE IF EXISTS `categorias`;
+
+CREATE TABLE `categorias` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cor` varchar(255) DEFAULT NULL,
+  `titulo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
 
 CREATE TABLE `videos` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `descricao` varchar(255) DEFAULT NULL,
-  `titulo` varchar(255) DEFAULT NULL,
+  `titulo` varchar(150) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `categoria_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categoria_id` (`categoria_id`),
+  CONSTRAINT `fk_categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`)
 );
