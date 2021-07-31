df3d121b22a48bdbf4bc71ebc7b3361e6474aafe
Adriano Avelino <adriano-st777@hotmail.com>
2021-07-31 12:06:41 -0300

    adiciona endpoint GET /categorias/{categoriaId}/videos
    A rota irá buscar vídeos por id da categoria

--- src/main/java/br/com/alura/aluraflix/controller/CategoriaController.java
+++ src/main/java/br/com/alura/aluraflix/controller/CategoriaController.java
@@ -3,9 +3,12 @@ package br.com.alura.aluraflix.controller;
 import br.com.alura.aluraflix.controller.dto.CategoriaRequestAtualizar;
 import br.com.alura.aluraflix.controller.dto.CategoriaRequestSalvar;
 import br.com.alura.aluraflix.controller.dto.CategoriaResponse;
+import br.com.alura.aluraflix.controller.dto.VideoResponse;
 import br.com.alura.aluraflix.entity.Categoria;
 import br.com.alura.aluraflix.repository.CategoriaRepository;
+import br.com.alura.aluraflix.repository.VideoRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,42 +16,45 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/categorias")
 public class CategoriaController {
-    private final CategoriaRepository repository;
+    private final CategoriaRepository categoriaRepository;
+    private final VideoRepository videoRepository;
 
-    public CategoriaController(CategoriaRepository repository) {
-        this.repository = repository;
+    public CategoriaController(CategoriaRepository repository, VideoRepository videoRepository) {
+        this.categoriaRepository = repository;
+        this.videoRepository = videoRepository;
     }
-
 
     @GetMapping
     public Page<CategoriaResponse> buscarTodas(Pageable pageable) {
-        return this.repository.findAll(pageable).map(CategoriaResponse::new);
+        return this.categoriaRepository.findAll(pageable).map(CategoriaResponse::new);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<CategoriaResponse> buscarPorId(@PathVariable Long id) {
-        Optional<Categoria> categoriaOptional = this.repository.findById(id);
+        Optional<Categoria> categoriaOptional = this.categoriaRepository.findById(id);
         if (categoriaOptional.isPresent()) return ResponseEntity.ok(new CategoriaResponse(categoriaOptional.get()));
         return ResponseEntity.notFound().build();
     }
 
     @PostMapping
     public ResponseEntity<CategoriaResponse> salvar(@RequestBody @Valid CategoriaRequestSalvar categoriaRequest, UriComponentsBuilder uriBuilder) {
-        Categoria categoria = this.repository.save(categoriaRequest.converterParaCategoria());
+        Categoria categoria = this.categoriaRepository.save(categoriaRequest.converterParaCategoria());
         URI uri = uriBuilder.path("/v1/categorias/{id}").buildAndExpand(categoria.getId()).toUri();
         return ResponseEntity.created(uri).body(new CategoriaResponse(categoria));
     }
 
     @PutMapping
     public ResponseEntity<CategoriaResponse> atualizar(@RequestBody @Valid CategoriaRequestAtualizar categoriaRequest) {
-        Optional<Categoria> categoriaOptional = this.repository.findById(categoriaRequest.getId());
+        Optional<Categoria> categoriaOptional = this.categoriaRepository.findById(categoriaRequest.getId());
         if (categoriaOptional.isPresent()) {
-            this.repository.save(categoriaRequest.converterParaCategoria());
+            this.categoriaRepository.save(categoriaRequest.converterParaCategoria());
             return ResponseEntity.ok(new CategoriaResponse(categoriaRequest.converterParaCategoria()));
         }
         return ResponseEntity.notFound().build();
@@ -56,11 +62,21 @@ public class CategoriaController {
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<?> deletar(@PathVariable Long id) {
-        Optional<Categoria> categoriaOptional = this.repository.findById(id);
+        Optional<Categoria> categoriaOptional = this.categoriaRepository.findById(id);
         if (categoriaOptional.isPresent()) {
-            this.repository.delete(categoriaOptional.get());
+            this.categoriaRepository.delete(categoriaOptional.get());
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping(path = "/{id}/videos")
+    public Page<VideoResponse> buscarVideosPorCategoriaId(@PathVariable("id") Long categoriaId, Pageable pageable) {
+        List<VideoResponse> videosPorCategoriaId = this.videoRepository
+                .findAllByCategoriaId(categoriaId, pageable)
+                .stream()
+                .map(VideoResponse::new)
+                .collect(Collectors.toList());
+        return new PageImpl<>(videosPorCategoriaId);
     }
 }
--- src/main/java/br/com/alura/aluraflix/repository/VideoRepository.java
+++ src/main/java/br/com/alura/aluraflix/repository/VideoRepository.java
@@ -1,9 +1,12 @@
 package br.com.alura.aluraflix.repository;
 
 import br.com.alura.aluraflix.entity.Video;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {
+    Page<Video> findAllByCategoriaId(Long categoriaId, Pageable pageable);
 }
