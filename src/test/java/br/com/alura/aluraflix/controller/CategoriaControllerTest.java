305929b36f74a532c330278469069dba32f4cad3
Adriano Avelino <adriano-st777@hotmail.com>
2021-08-05 21:35:54 -0300

    adiciona testes de controler para categoria
    adiciona teste para atualizar e deletar categoria, e também, para buscar 
    videos por id de categoria

--- src/test/java/br/com/alura/aluraflix/controller/CategoriaControllerTest.java
+++ src/test/java/br/com/alura/aluraflix/controller/CategoriaControllerTest.java
@@ -1,34 +1,19 @@
 package br.com.alura.aluraflix.controller;
 
 import br.com.alura.aluraflix.entity.Categoria;
-import br.com.alura.aluraflix.entity.Video;
 import br.com.alura.aluraflix.repository.CategoriaRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -81,6 +66,47 @@ class CategoriaControllerTest {
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
+    }
+
+    @Test
+    void deveAtualizarUmaCategoria() throws Exception {
+        Categoria categoriaModificada = new Categoria(1l, "LIVRE alterada", "#eeeccc");
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .put("/v1/categorias")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(categoriaModificada));
+
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void deveDeletarUmaCategoria() throws Exception {
+        Categoria categoria = repository.save(new Categoria("Categoria1", "#001100"));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .delete("/v1/categorias/{id}", categoria.getId())
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void deveBuscarVideosPorCategoriaId() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/v1/categorias/{id}", 1l)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
     }
 
