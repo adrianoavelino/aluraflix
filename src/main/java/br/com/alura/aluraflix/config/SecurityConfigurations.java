2b8566b9b865188747f15e9ddbf601060d5f7179
Adriano Avelino <adriano-st777@hotmail.com>
2021-08-07 09:13:20 -0300

    adiciona teste com liberação para requisições GET /v1/videos

--- src/main/java/br/com/alura/aluraflix/config/SecurityConfigurations.java
+++ src/main/java/br/com/alura/aluraflix/config/SecurityConfigurations.java
@@ -1,10 +1,31 @@
 package br.com.alura.aluraflix.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/v1/videos").permitAll()
+                .antMatchers(HttpMethod.GET, "/v1/videos/").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 }
