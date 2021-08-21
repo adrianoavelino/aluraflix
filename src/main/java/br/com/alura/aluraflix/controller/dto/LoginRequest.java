9514e4cded64e5734d155f574bbdf6d564cb3b95
Adriano Avelino <adriano-st777@hotmail.com>
2021-08-21 15:16:25 -0300

    adiciona header com token nos teste de CategoriaControllerTest

--- src/main/java/br/com/alura/aluraflix/controller/dto/LoginRequest.java
+++ src/main/java/br/com/alura/aluraflix/controller/dto/LoginRequest.java
@@ -6,6 +6,14 @@ public class LoginRequest {
     private String email;
     private String senha;
 
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
     public String getEmail() {
         return email;
     }
--- src/test/java/br/com/alura/aluraflix/controller/CategoriaControllerTest.java
+++ src/test/java/br/com/alura/aluraflix/controller/CategoriaControllerTest.java
@@ -1,19 +1,25 @@
 package br.com.alura.aluraflix.controller;
 
+import br.com.alura.aluraflix.controller.dto.LoginRequest;
 import br.com.alura.aluraflix.entity.Categoria;
 import br.com.alura.aluraflix.repository.CategoriaRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.UnsupportedEncodingException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,11 +34,34 @@ class CategoriaControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private HttpHeaders getHeader() throws Exception {
+        LoginRequest loginRequest = new LoginRequest("admin@email.com", "123456");
+        String loginRequestJson = objectMapper.writeValueAsString(loginRequest);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(loginRequestJson);
+
+        MvcResult result = mockMvc.perform(request)
+                .andReturn();
+
+        return getHttpHeaderAutenticado(result);
+    }
+
+    private HttpHeaders getHttpHeaderAutenticado(MvcResult result) throws UnsupportedEncodingException {
+        String response = result.getResponse().getContentAsString();
+        String token = JsonPath.read(response, "$.token");
+        HttpHeaders header = new HttpHeaders();
+        header.setBearerAuth(token);
+        return header;
+    }
+
     @Test
     void deveBuscarTodasCategorias() throws Exception {
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .get("/v1/categorias")
-                .contentType(MediaType.APPLICATION_JSON);
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(this.getHeader());
 
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
@@ -45,7 +74,8 @@ class CategoriaControllerTest {
         Categoria categoria = repository.findById(1l).get();
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .get("/v1/categorias/{id}", categoria.getId())
-                .contentType(MediaType.APPLICATION_JSON);
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(this.getHeader());
 
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
@@ -61,7 +91,8 @@ class CategoriaControllerTest {
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .post("/v1/categorias")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(categoria));
+                .content(objectMapper.writeValueAsString(categoria))
+                .headers(this.getHeader());
 
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
@@ -76,7 +107,8 @@ class CategoriaControllerTest {
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .put("/v1/categorias")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(categoriaModificada));
+                .content(objectMapper.writeValueAsString(categoriaModificada))
+                .headers(this.getHeader());
 
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
@@ -90,7 +122,8 @@ class CategoriaControllerTest {
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .delete("/v1/categorias/{id}", categoria.getId())
-                .contentType(MediaType.APPLICATION_JSON);
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(this.getHeader());
 
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
@@ -102,7 +135,8 @@ class CategoriaControllerTest {
     void deveBuscarVideosPorCategoriaId() throws Exception {
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .get("/v1/categorias/{id}", 1l)
-                .contentType(MediaType.APPLICATION_JSON);
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(this.getHeader());
 
         mockMvc.perform(request)
                 .andDo(MockMvcResultHandlers.print())
